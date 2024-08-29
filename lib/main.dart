92e456f021bbb9a5422449bb8eb0de4ebe7726a3
Corbin Meier <corbinivon@gmail.com>
2024-08-29 16:18:44 -0700

    fix(main): Ensure proper initialization and clean up debug logs
    - Added `WidgetsFlutterBinding.ensureInitialized()` to ensure that the Flutter framework is fully initialized before the app starts.
    - Removed debug logging statements from `main.dart` to clean up the code and prepare it for production.
    - Ensured that the app's startup and widget build processes are properly handled without unnecessary logging.

--- lib/main.dart
+++ lib/main.dart
@@ -3,36 +3,55 @@ import 'package:flutter_os_wear/screens/ambient_screen.dart';
 import 'package:flutter_os_wear/screens/start_screen.dart';
 import 'package:flutter_os_wear/wear.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // Ensure the Flutter framework is properly initialized
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Add some logging to trace the app's startup process
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Flutter Wear App',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: WatchScreen(),
-        debugShowCheckedModeBanner: false,
-      );
+  Widget build(BuildContext context) {
+    // Log when MyApp is being built
+
+    return MaterialApp(
+      title: 'Flutter Wear App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: WatchScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
 
 class WatchScreen extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => WatchShape(
-    key: Key('watch_shape_key'), // Added key for WatchShape
-    builder: (context, shape) => InheritedShape(
-      key: Key('inherited_shape_key'), // Added key for InheritedShape
-      shape: shape,
-      child: AmbientMode(
-        key: Key('ambient_mode_key'), // Added key for AmbientMode
-        builder: (context, mode) =>
-          mode == Mode.active ? StartScreen() : AmbientWatchFace(),
-        update: () {
-          // Add your update logic here
-          // This could involve refreshing the UI or handling some other aspect of the app's state
-        },
-      ),
-    ),
-  );
+  Widget build(BuildContext context) {
+    // Log when WatchScreen is being built
+
+    return WatchShape(
+      key: Key('watch_shape_key'),
+      builder: (context, shape) {
+
+        return InheritedShape(
+          key: Key('inherited_shape_key'),
+          shape: shape,
+          child: AmbientMode(
+            key: Key('ambient_mode_key'),
+            builder: (context, mode) {
+
+              return mode == Mode.active ? StartScreen() : AmbientWatchFace();
+            },
+            update: () {
+              // Log when the AmbientMode's update method is triggered
+            },
+          ),
+        );
+      },
+    );
+  }
 }
